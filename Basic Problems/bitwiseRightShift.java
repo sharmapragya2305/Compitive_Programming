90b9da78b45f5f56da2349ed107e440b8b0d350a
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-04-09 11:06:13 +0530

    Create bitwiseRightShift.java
    Given two positive integers a and b. Your task is to perform right shift bitwise operation on it .

---
+++ Basic Problems/bitwiseRightShift.java
@@ -0,0 +1,22 @@
+/* Given two positive integers a and b. Your task is to perform right shift bitwise operation on it as given below:
+Do a>>b.
+ 
+
+Example 1:
+
+Input:
+a = 5
+b = 2
+Output: 
+1 
+Explanation: 5>>2 = 1. */
+
+
+public static void utility(int a, int b){
+
+    //just complete below statement
+    int ans = a>>b;
+
+    //print the result
+    System.out.println(ans);
+}
