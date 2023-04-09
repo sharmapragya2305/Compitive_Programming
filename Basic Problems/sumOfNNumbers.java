e085bcf5844f09078fe3f81af1a6b98559cb23e6
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-04-09 10:58:36 +0530

    Create sumOfNNumbers.java
    Given an integer N find the sum of the first N natural number.

---
+++ Basic Problems/sumOfNNumbers.java
@@ -0,0 +1,25 @@
+/*Given an integer N find the sum of the first N natural number.
+
+Example 1:
+
+Input:
+N = 10
+Output: 
+55
+Explanation:
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55.*/
+
+
+
+public static int nSum(int n){
+    int ans = 0;
+
+    //Write your code here to calculate and return sum of n number.
+    for(int i=1 ; i<=n; i++){
+        ans = ans+i;
+    }
+    
+    
+    
+    return ans;
+}
