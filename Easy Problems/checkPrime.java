b76106e93b9db35946f1b379a3868506321fc361
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-04-23 22:04:29 +0530

    Create checkPrime.java
    Given an integer n check if n is prime or not.
    A prime number is a number that is divisible by 1 and itself only.

---
+++ Easy Problems/checkPrime.java
@@ -0,0 +1,30 @@
+/* Given an integer n check if n is prime or not.
+A prime number is a number that is divisible by 1 and itself only.
+
+Example 1:
+
+Input:
+n = 17
+Output: 
+True
+Explanation:
+17 is  divisible by  only 1 and 17.
+So it's a prime number. */
+
+public static boolean prime(int n){
+    
+    //Write your code here
+    //returns a boolean value 
+    if (n==1)
+    return false;
+    if (n==2 || n==3)
+    return true;
+    if(n%2==0 || n%3==0)
+    return false;
+    for(int i=5 ; i*i<=n; i=i+6)
+    if(n%i==0|| n%(i+2)==0)
+    return false;
+    return true;
+    
+    
+}
