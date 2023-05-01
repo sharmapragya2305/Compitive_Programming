33eca6acfad3d6d1fec1949a80b798f409230389
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-05-01 11:15:05 +0530

    Create factorial.java
    Given an integer N. Write a program to return the factorial of N.

---
+++ Basic Problems/factorial.java
@@ -0,0 +1,23 @@
+/* Given an integer N. Write a program to return the factorial of N.
+Note: 0 factorial is equal to 1.
+
+Example 1:
+
+Input:
+N = 10
+Output: 
+3628800
+Explanation:
+1*2*3*4*5*6*7*8*9*10 = 3628800 . */
+
+public static int nFactorial(int n){
+    int ans = 1;
+    
+    //Write your code here
+    while (n>0){
+        ans = ans * n;
+        n--;
+    }
+    
+    return ans;
+}
