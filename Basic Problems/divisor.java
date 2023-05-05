6a9a1d257f6120f1f1d1f90ac5c6f8c046c62eb3
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-05-05 21:53:53 +0530

    Create divisor.java
    Given an integer N. Write a program to print all the divisors of N.

---
+++ Basic Problems/divisor.java
@@ -0,0 +1,20 @@
+/* Given an integer N. Write a program to print all the divisors of N.
+
+Example 1:
+
+Input:
+N = 12
+Output: 
+1 2 3 4 6 12
+Explanation:
+12 is divisible by 1 2 3 4 6 12.  */
+
+public static void divisor(int n){
+        
+    //Write your code here
+    for(int i=1;i<=n;i++){
+        if(n%i==0)
+        System.out.print(i+" ");
+    }
+        
+}
