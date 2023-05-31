e7529cde03f76ce1d48d3f593824e3a210c25c88
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-05-31 20:59:48 +0530

    Create checkIfArrayIsSorted.java

---
+++ Easy Problems/checkIfArrayIsSorted.java
@@ -0,0 +1,22 @@
+/* Given an array arr[] of size N, check if it is sorted in non-decreasing order or not. 
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 1
+Explanation: The given array is sorted. */
+
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        for(int i = 0; i<n-1;i++){
+            if(arr[i+1] < arr[i]){
+                return false;
+            }
+             
+        }
+        return true;
+    }
+}
