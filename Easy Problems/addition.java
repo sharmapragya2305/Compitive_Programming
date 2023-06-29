df8a8f1a06d5a8907d17ef68e7d03cf11fcf7e32
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-06-29 18:25:00 +0530

    Create addition.java

---
+++ Easy Problems/addition.java
@@ -0,0 +1,18 @@
+/*Design a class as described.
+
+class name : Addition
+function: 1
+      function name : add
+      parameters : a(int), b(int)
+      return type: int
+      access specifier: public
+      static: yes
+      task: returns the sum of the values 
+            given in the parameter. */
+
+class Addition{
+    public static int add(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+}
