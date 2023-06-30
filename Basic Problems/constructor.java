e7726248a5052d96efdfbe9b9033c79ad5c8de30
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-06-30 20:16:20 +0530

    Create constructor.java

---
+++ Basic Problems/constructor.java
@@ -0,0 +1,16 @@
+/* Design a class as described below.
+
+class name : User
+instance variable: name(String)
+constructor: 1
+             parameter: none
+             task: initialize the instance
+             variable to "Default" */
+
+class User{
+    String name;
+    public User(){
+        this.name="Default";
+    }
+    
+}
