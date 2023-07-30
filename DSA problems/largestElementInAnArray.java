ff256608606489fde8f09eb7009fc462e35bff39
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-07-30 10:50:53 +0530

    Create largestElementInAnArray.java

---
+++ DSA problems/largestElementInAnArray.java
@@ -0,0 +1,25 @@
+/* Given an array A[] of size n. The task is to find the largest element in it.
+ 
+
+Example 1:
+
+Input:
+n = 5
+A[] = {1, 8, 7, 56, 90}
+Output:
+90
+Explanation:
+The largest element of given array is 90. */
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        for(int i=0;i<n;i++){
+            Arrays.sort(arr);
+            return arr[n-1];
+        }
+        return -1;
+        
+    }
+}
