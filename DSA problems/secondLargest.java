482e739151086dec7d27bc5dd2f5f1cfc8be8a67
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-08-01 09:06:32 +0530

    Create secondLargest.java

---
+++ DSA problems/secondLargest.java
@@ -0,0 +1,30 @@
+/* Given an array Arr of size N, print second largest distinct element from an array.
+
+Example 1:
+
+Input: 
+N = 6
+Arr[] = {12, 35, 1, 10, 34, 1}
+Output: 34
+Explanation: The largest element of the 
+array is 35 and the second largest element
+is 34. */
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        int largest = arr[0];
+        int slargest = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>slargest){
+        slargest=arr[i];
+            }
+        }
+        return slargest;
+    }
+    
+}
