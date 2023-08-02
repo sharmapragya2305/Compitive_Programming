c5c9edf2a29562bb21cf389b768d05c55fc4a11a
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-08-02 20:04:22 +0530

    Create arrayInsertAtIndex.java

---
+++ DSA problems/arrayInsertAtIndex.java
@@ -0,0 +1,36 @@
+/* Insertion is a basic but frequently used operation. Arrays in most languages cannnot be dynamically shrinked or expanded. Here, we will work with such arrays and try to insert an element at some index.
+
+You are given an array arr(0-based index). The size of the array is given by sizeOfArray. You need to insert an element at given index.
+
+Example 1:
+
+Input:
+sizeOfArray = 6
+arr[] = {1, 2, 3, 4, 5}
+index = 5, element = 90
+Output: 1 2 3 4 5 90
+Explanation: 90 is inserted at index
+5(0-based indexing). After inserting,
+array elements are like
+1, 2, 3, 4, 5, 90. */
+
+class Solution
+{
+    // You need to insert the given element at the given index. 
+    // After inserting the elements at index, elements
+    // from index onward should be shifted one position ahead
+    // You may assume that the array already has sizeOfArray - 1
+    // elements.
+    public void insertAtIndex(int arr[],int sizeOfArray,int index,int element)
+    {
+        //Your code here, Geeks
+        for(int i=sizeOfArray-1;i>index;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[index]=element;
+        
+        
+    }
+
+
+}
