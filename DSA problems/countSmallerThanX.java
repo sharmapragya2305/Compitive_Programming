da8f6e79ef022046828092faf7159da116227333
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-08-05 10:19:18 +0530

    Create countSmallerThanX.java

---
+++ DSA problems/countSmallerThanX.java
@@ -0,0 +1,27 @@
+/* Given an unsorted array arr[] of size N containing non-negative integers. You will also be given an integer X, the task is to count the number of elements which are strictly smaller than X. The given integer may or not be present in the array given.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {4 5 3 1 2}
+X = 3
+Output: 2
+Explanation: Here X = 3, and elements that
+are smaller than X are 1 and 2.
+ */
+
+class Solution
+{
+    public static int smallerThanX(int arr[], int n, int x)
+    {
+        // Your code here
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(x>arr[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
