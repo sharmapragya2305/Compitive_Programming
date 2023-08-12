681e4bde5307780f90c279c3ad109c3c33a8fbcf
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-08-12 22:03:20 +0530

    Update secondLargest.java

--- DSA problems/secondLargest.java
+++ DSA problems/secondLargest.java
@@ -14,17 +14,17 @@ class Solution {
     int print2largest(int arr[], int n) {
         // code here
         int largest = arr[0];
-        int slargest = -1;
-        for(int i=0;i<n;i++){
+        int secondlargest = -1;
+        for (int i=0;i<n;i++){
             if(arr[i]>largest){
-            slargest=largest;
-            largest=arr[i];
+                secondlargest = largest;
+                largest = arr[i];
+                
             }
-            else if(arr[i]<largest && arr[i]>slargest){
-        slargest=arr[i];
+            else if(arr[i]!=largest && arr[i]>secondlargest){
+            secondlargest = arr[i];
             }
         }
-        return slargest;
+        return secondlargest;
     }
-    
 }
