e9801b375f34ae93e96d1ed5f366d765ecb19861
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-08-20 17:09:47 +0530

    Create findTheElementThatAppearOnce.java

---
+++ DSA problems/findTheElementThatAppearOnce.java
@@ -0,0 +1,25 @@
+/* Given a sorted array A[] of N positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+
+Example 1:
+
+Input:
+N = 5
+A = {1, 1, 2, 5, 5}
+Output: 2
+Explanation: 
+Since 2 occurs once, while
+other numbers occur twice, 
+2 is the answer. */
+
+class Sol
+{
+    public static int search(int A[], int N)
+    {
+        // your code here
+        int XOR = 0;
+        for(int i=0;i<N;i++){
+            XOR = XOR ^ A[i];
+        }
+        return XOR;
+    }
+}
