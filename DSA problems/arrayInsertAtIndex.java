249fcc1d92ab21566a72d5323b2f8b1b7895417d
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2023-09-01 10:14:31 +0530

    Update arrayInsertAtIndex.java

--- DSA problems/arrayInsertAtIndex.java
+++ DSA problems/arrayInsertAtIndex.java
@@ -2,7 +2,7 @@
 
 You are given an array arr(0-based index). The size of the array is given by sizeOfArray. You need to insert an element at given index.
 
-Example 1:
+Example 2:
 
 Input:
 sizeOfArray = 6
