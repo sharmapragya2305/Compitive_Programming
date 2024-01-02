e4aeaa290c6c1a85512e4f57cc8ead87487f75f7
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2024-01-02 21:57:30 +0530

    Create countDigits.java

---
+++ DSA problems/countDigits.java
@@ -0,0 +1,25 @@
+/*  Given a natural number N. You have to find the number of digits in it.
+
+Note: New line will be added by the driver code
+
+Example 1:
+
+Input:
+n = 5534
+Output:
+4
+Explanation: 5534 has 4 digits  */
+
+public static void countDigits(int n){
+
+    //write your code here
+    //print number of digits in n
+    int count = 0;
+    int temp = n;
+    while(temp!=0){
+        temp= temp/10;
+        count++;
+    }
+    System.out.print(count);
+
+}
