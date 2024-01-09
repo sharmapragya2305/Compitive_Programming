b7f47bf046b5a977438b78264a154c4717dbbab0
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2024-01-09 13:25:36 +0530

    Update countElementGreaterThanX.java

--- DSA problems/countElementGreaterThanX.java
+++ DSA problems/countElementGreaterThanX.java
@@ -1,6 +1,6 @@
 /* Given an unsorted array arr[] of size N containing non-negative integers. You will also be given an integer X, the task is to count the number of elements which are strictly greater than X. The given integer may or not be present in the array given.
 
-Example 1:
+Example 2:
 
 Input:
 N = 5
