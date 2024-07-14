30c1b1c83d1b706e8776cbde48c51415c04087e0
Pragya Sharma <92048007+sharmapragya2305@users.noreply.github.com>
2024-07-14 16:05:45 +0530

    Create swapTwoNumbers.java

---
+++ dailyProblems/swapTwoNumbers.java
@@ -0,0 +1,19 @@
+/* Swap given two numbers and print them. (Try to do it without a temporary variable.) and return it. 
+Example 1:
+
+Input: a = 13, b = 9
+Output: 9 13
+Explanation: after swapping it
+becomes 9 and 13
+*/
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+    List<Integer> res=new ArrayList<>();
+    res.add(b);
+    res.add(a);
+    return res;
+        
+    }
+}
